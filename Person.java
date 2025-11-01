fccd1908c07c765b12b88967db59e418055df71c
NotKiwy <c3zYyyyx.offical@gmail.com>
2025-11-01 11:42:33 +0300

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person {
+    private String name;
+    private String email;
+    private int age;
+
+    public Person(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public void printInfo() {
+        System.out.println("==========");
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Age: " + age);
+    }
+}
